474ccf19d9cf2304276378fd20b27e90b0cae1df
havaacigse <havvacigses@gmail.com>
2025-09-26 19:18:56 +0300

    ilk commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-active');
+
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+
+        burger.classList.toggle('toggle');
+    });
+
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('nav-active')) {
+                nav.classList.remove('nav-active');
+                burger.classList.remove('toggle');
+                navLinks.forEach(item => item.style.animation = '');
+            }
+        });
+    });
+}
+
+navSlide();
